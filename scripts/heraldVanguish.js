9235799c5927508e5523824df7118d8752f8cbc8
Yeremia Yoga <yogayeremia@gmail.com>
2025-03-26 14:51:58 +0700

    first commit

--- scripts/heraldVanguish.js
+++ scripts/heraldVanguish.js
@@ -546,19 +546,29 @@ Hooks.on("updateActor", async (actor, data) => {
     if (npcTokenFlag) {
       if (game.user.isGM) {
         if (npcTokenFlag.toughness <= 0) {
-       
-          let chatContent = `
-          ${actor.name} is now Weakness Broken!
-          <br>
-          "${actor.name} has a [[-4]] penalty to their <b>Armor Class</b> and <b>Damage Rolls</b>, and a [[-2]] penalty to all <b>Saving Throws, Ability Checks, Attack Rolls, and Spell Save DC</b>, alongside their movement speed will drop by 25%, lasting until the end of their next turn.
-          <br>
-          The ${actor.name} gains a stack of <b>Exhaustion</b> which does not get nullified at the end of Weakness Broken."
-        `;
+          let existingEffect = actor.effects.find(
+            (e) => e.name === "Weakness Broken"
+          );
 
-          ChatMessage.create({
-            content: chatContent,
-            speaker: ChatMessage.getSpeaker({ token: tokenDocument }),
-          });
+          if (!existingEffect) {
+
+
+
+            let chatContent = `
+            ${actor.name} is now Weakness Broken!
+            <br>
+            "${actor.name} has a [[-4]] penalty to their <b>Armor Class</b> and <b>Damage Rolls</b>, and a [[-2]] penalty to all <b>Saving Throws, Ability Checks, Attack Rolls, and Spell Save DC</b>, alongside their movement speed will drop by 25%, lasting until the end of their next turn.
+            <br>
+            The ${actor.name} gains a stack of <b>Exhaustion</b> which does not get nullified at the end of Weakness Broken."`;
+
+            ChatMessage.create({
+              content: chatContent,
+              speaker: ChatMessage.getSpeaker({ token: tokenDocument }),
+            });
+          }
+     
+
+        
         }
       }
     }
