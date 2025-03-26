efd7f093771dfb345f6cf7a0f2cce9435634cb0a
Yeremia Yoga <yogayeremia@gmail.com>
2025-03-26 16:56:39 +0700

    first commit

--- scripts/heraldVanguish.js
+++ scripts/heraldVanguish.js
@@ -551,9 +551,86 @@ Hooks.on("updateActor", async (actor, data) => {
           );
 
           if (!existingEffect) {
-
-
-
+            await actor.createEmbeddedDocuments("ActiveEffect", [
+              {
+                name: "Weakness Broken",
+                icon: "icons/magic/death/skull-energy-light-white.webp",
+                changes: [
+                  {
+                    key: "system.attributes.ac.bonus",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -4,
+                  },
+                  {
+                    key: "system.bonuses.All-Damage",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -4,
+                  },
+                  {
+                    key: "system.bonuses.All-Attacks",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -2,
+                  },
+                  {
+                    key: "system.bonuses.abilities.check",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -2,
+                  },
+                  {
+                    key: "system.bonuses.abilities.save",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -2,
+                  },
+                  {
+                    key: "system.bonuses.spell.dc",
+                    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+                    value: -2,
+                  },
+                  {
+                    key: "system.attributes.movement.walk",
+                    mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY,
+                    value: 0.75,
+                  },
+                  {
+                    key: "system.attributes.movement.burrow",
+                    mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY,
+                    value: 0.75,
+                  },
+                  {
+                    key: "system.attributes.movement.climb",
+                    mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY,
+                    value: 0.75,
+                  },
+                  {
+                    key: "system.attributes.movement.fly",
+                    mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY,
+                    value: 0.75,
+                  },
+                  {
+                    key: "system.attributes.movement.swim",
+                    mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY,
+                    value: 0.75,
+                  },
+                ],
+                origin: `Actor.${actor.id}`,
+                disabled: false,
+                transfer: false,
+                duration: {
+                  rounds: 2,
+                  turns: 1,
+                  specialDuration: ["turnEnd", "combatEnd", "zeroHP"],
+                },
+                flags: {
+                  core: {
+                    statusId: "Weakness Broken",
+                  },
+                  dae: {
+                    specialDuration: ["turnEnd", "combatEnd", "zeroHP"],
+                  },
+                  "temp-effect": true,
+                },
+              },
+            ]);
             let chatContent = `
             ${actor.name} is now Weakness Broken!
             <br>
@@ -565,10 +642,23 @@ Hooks.on("updateActor", async (actor, data) => {
               content: chatContent,
               speaker: ChatMessage.getSpeaker({ token: tokenDocument }),
             });
-          }
-     
 
-        
+            new Sequence()
+              .sound()
+              .file(
+                "/modules/herald-vanguish-beta/assets/sound/weaknessBroken_apply.ogg"
+              )
+              .volume(0.1)
+              .baseEffect({ type: "reverb", intensity: 4 })
+              .audioChannel("music")
+              .play();
+            let exhaustion = actor.system.attributes.exhaustion || 0;
+            let newExhaustionLevel = Math.min(exhaustion - 0, 10);
+            newExhaustionLevel = Math.min(exhaustion + 1, 10);
+            await actor.update({
+              "system.attributes.exhaustion": newExhaustionLevel,
+            });
+          }
         }
       }
     }
